8235a96f264368eda841fc581eed63f56f5d05e1
Daniel Lamando <dan@danopia.net>
2020-11-26 23:03:54 +0100

    blog-manager: Relocate firebase deploy code in module

--- publishing/blog-manager/routines/publish-blog.ts
+++ publishing/blog-manager/routines/publish-blog.ts
@@ -1,102 +1,18 @@
 import { BlobEntry, Entry, FolderEntry } from "https://uber.danopia.net/deno/dust@v1beta1/skylink/src/mod.ts";
 import { AutomatonBuilder, Automaton, ApiHandle } from "https://uber.danopia.net/deno/dust@v1beta1/client-automaton/mod.ts";
+
 import { ServiceAccount } from "https://danopia.net/deno/google-service-account@v1.ts";
+import { deployFirebaseSite } from "https://danopia.net/deno/firebase-hosting-deploy@v1.ts";
 
 import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 import Mustache from 'https://deno.land/x/mustache@v0.2.1/mustache.mjs';
-import * as Gzip from "https://deno.land/x/wasm_gzip@v1.0.0/mod.ts";
-import {Sha256} from "https://deno.land/std@0.78.0/hash/sha256.ts";
 
 type SiteFile = {path: string, body: Uint8Array};
 async function publishFirebaseSite(siteId: string, credentialPath: string, files: Iterable<SiteFile>) {
   const credential = await ServiceAccount.readFromFile(credentialPath);
   const token = await credential.issueToken("https://www.googleapis.com/auth/firebase");
-
-  const {name, status} = await fetch(
-    'https://firebasehosting.googleapis.com/v1beta1/sites/'+siteId+'/versions', {
-      method: 'POST',
-      body: JSON.stringify({
-        // "config": {
-        //   "headers": [{
-        //     "glob": "**",
-        //     "headers": {
-        //       "Cache-Control": "max-age=1800"
-        //     }
-        //   }]
-        // }
-      }),
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(x => x.json()) as {name: string; status: string};
-  console.log('Firebase release', name, 'is', status);
-
-  const fileHashes: Record<string,string> = Object.create(null);
-  const hashMap = new Map<string,SiteFile&{compressed: Uint8Array}>();
-  for (const file of files) {
-    const compressed = Gzip.gzipEncode(file.body);
-    const hash = new Sha256().update(compressed).hex();
-    hashMap.set(hash, {...file, compressed});
-    fileHashes[file.path] = hash;
-  }
-
-  let {uploadRequiredHashes, uploadUrl} = await fetch(
-    `https://firebasehosting.googleapis.com/v1beta1/${name}:populateFiles`, {
-      method: 'POST',
-      body: JSON.stringify({
-        files: fileHashes,
-      }),
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(x => x.json()) as {uploadRequiredHashes: string[], uploadUrl: string};
-  uploadRequiredHashes = uploadRequiredHashes ?? [];
-  console.log('Firebase wants', uploadRequiredHashes.length, 'files out of', hashMap.size);
-
-  for (const requiredHash of uploadRequiredHashes) {
-    const file = hashMap.get(requiredHash);
-    if (!file) throw new Error(`BUG: firebase wanted hash ${requiredHash} which we didn't offer`);
-
-    const resp = await fetch(uploadUrl+'/'+requiredHash, {
-      method: 'POST',
-      body: file.compressed,
-      headers: {
-        'content-type': /*file.mime ??*/ 'application/octet-stream',
-        authorization: `Bearer ${token.access_token}`,
-      },
-    });
-    if (resp.status !== 200) throw new Error(`Firebase file upload returned ${resp.status}`);
-    const compRatio = (file.body.length - file.compressed.length) / file.body.length;
-    console.log('Uploaded', file.path, '-', Math.round(compRatio * 100), '% compression');
-  }
-
-  const release = await fetch(
-    `https://firebasehosting.googleapis.com/v1beta1/${name}?update_mask=status`, {
-      method: 'PATCH',
-      body: JSON.stringify({
-        status: 'FINALIZED',
-      }),
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(x => x.json());
-  console.log('Completed Firebase release:', release);
-
-  const deploy = await fetch(
-    `https://firebasehosting.googleapis.com/v1beta1/sites/${siteId}/releases?${new URLSearchParams([['versionName', name]])}`, {
-      method: 'POST',
-      // body: JSON.stringify({
-      //   status: 'FINALIZED',
-      // }),
-      headers: {
-        // 'content-type': 'application/json',
-        authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(x => x.json());
-  console.log('Completed Firebase deploy:', deploy);
+  const release = await deployFirebaseSite(siteId, token.access_token, files);
+  return release.name;
 }
 
 const renderMustache = Mustache.render as unknown as (template: string, view: unknown) => string;
